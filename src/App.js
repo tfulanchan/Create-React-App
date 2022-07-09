1c5b978e8bd1aaf5a1444a3b1b5dba993b63bb38
tfulanchan <60479975+tfulanchan@users.noreply.github.com>
2022-07-09 19:04:45 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -17,9 +17,7 @@ function App() {
         >
           Click here to learn about me
         </a>
-        <p>
-          Do you know <code>me</code>? 
-        </p>
+
         let arrys = ['a','b'];
         arrys[2] = 1;
         console.log(arrys);
