39d19472442b5252728d70dc4471596d3c55312f
tfulanchan <60479975+tfulanchan@users.noreply.github.com>
2022-07-09 14:16:13 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Do you know <code>me</code>? 
         </p>
         <a
           className="App-link"
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Click here to learn about me
         </a>
       </header>
     </div>
