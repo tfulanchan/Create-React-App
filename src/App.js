54eaa1f2cd49522b2a3292417ac58c07ba098869
tfulanchan <60479975+tfulanchan@users.noreply.github.com>
2022-07-10 20:44:22 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -4,14 +4,10 @@ import './App.css';
 function App() {
   return (
  
-<div class="css-1vcfx3l">
-<h3 class="css-1xm4gxl">Declarative</h3>
-<div>
-<p>React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.</p>
+ <h3 class="css-1xm4gxl">Declarative</h3>
+ <p>React makes it painless to create interactive UIs. Design simple views for each state in your application, and React will efficiently update and render just the right components when your data changes.</p>
 <p>Declarative views make your code more predictable and easier to debug.</p>
-</div>
-</div>
-	  
+ 	  
     <div className="App">
       <header className="App-header">
 	<h2><code>Chan</code></h2>
