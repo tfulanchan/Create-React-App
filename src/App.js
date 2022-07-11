b73a72f8eb69dd45c8eb6959734a4cdbca860b08
tfulanchan <60479975+tfulanchan@users.noreply.github.com>
2022-07-10 21:10:00 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -11,8 +11,8 @@ function App() {
   <label for="fname">First name:</label><br>
   <input type="text" id="fname" name="fname" value="John"><br>
   <label for="lname">Last name:</label><br>
-  <input type="text" id="lname" name="lname" value="Doe"><br><br>
-  <input type="submit" value="Submit">
+  <input type="text" id="lname" name="lname" value="Doe"></input><br><br>
+  <input type="submit" value="Submit"></input>
 </form> 
 
 <p>If you click the "Submit" button, the form-data will be sent to a page called "/action_page.php".</p>
