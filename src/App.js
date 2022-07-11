3423c8bf88699f24ec6e85c6618942ea65ab8377
tfulanchan <60479975+tfulanchan@users.noreply.github.com>
2022-07-10 21:11:22 -0400

    Update App.js

--- src/App.js
+++ src/App.js
@@ -9,9 +9,9 @@ function App() {
 	  <h2><code>Chan</code></h2>
 <form >
   <label for="fname">First name:</label><br>
-  <input type="text" id="fname" name="fname" value="John"><br>
-  <label for="lname">Last name:</label><br>
-  <input type="text" id="lname" name="lname" value="Doe"></input><br><br>
+  <input type="text" id="fname" name="fname" value="John">
+  <label for="lname">Last name:</label>
+  <input type="text" id="lname" name="lname" value="Doe"></input>
   <input type="submit" value="Submit"></input>
 </form> 
 
